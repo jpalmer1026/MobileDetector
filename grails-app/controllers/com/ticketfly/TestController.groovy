c0cdc83f9a3f17544089a46252fc0a50f1327540
Jeff Palmer <palmer1026@gmail.com>
2014-04-17 16:15:41 -0500

    Added additional comments to TestController

--- grails-app/controllers/com/ticketfly/TestController.groovy
+++ grails-app/controllers/com/ticketfly/TestController.groovy
@@ -4,13 +4,13 @@ class TestController {
 
     def mobileService
 
-    def afterInterceptor = [action: this.&handleMobile]
+    def afterInterceptor = [action: this.&handleMobile] // using interceptor instead of filter because of [GRAILS-6555]
 
     private handleMobile(model, modelAndView) {
 
-        if (modelAndView && mobileService.detect(request)) {  // use mobileAndView to change the view for mobile
+        if (modelAndView && mobileService.detect(request)) {  // use modelAndView to change the view for mobile
 
-            modelAndView.viewName = '/mobile/' + modelAndView.viewName
+            modelAndView.viewName = '/mobile/' + modelAndView.viewName // mobile views will be put under mobile directory
 
         }
 
